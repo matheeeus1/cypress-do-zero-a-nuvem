1a7d63b4d263a7a4dc80191f13b65c6138289ea4
matheeeus1 <matheeus.pascoal@gmail.com>
2025-02-06 18:35:36 -0300

    Implementa testes seção 5

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -86,4 +86,18 @@ describe('Central de Atendimento ao Cliente TAT   ', () => {
       .select(1)
       .should('have.value', 'blog')
   })
+  it('marca o tipo de atendimento "Feedback"', () => {
+    cy.get('input[type="radio"][value="feedback"]')
+      .check()
+      .should('be.checked')
+  })
+  it.only('marca cada tipo de atendimento"', () => {
+    cy.get('input[type="radio"]')
+      .each(typeOfService => {
+        cy.wrap(typeOfService)
+          .check()
+          .should('be.checked')
+      })
+  })
+
 })
